2108e6c9b666281d06dd402984d7028150dfdf26
frankcl <frank.lichen@gmail.com>
2022-11-01 20:01:55 +0800

    split ONSReceiver and ONSConsumer

--- stream-receiver/src/main/java/com/manong/stream/boost/receiver/ons/ONSReceiver.java
+++ stream-receiver/src/main/java/com/manong/stream/boost/receiver/ons/ONSReceiver.java
@@ -2,16 +2,14 @@ package com.manong.stream.boost.receiver.ons;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.aliyun.openservices.ons.api.*;
 import com.manong.stream.sdk.receiver.Receiver;
-import com.manong.weapon.aliyun.common.RebuildManager;
 import com.manong.weapon.aliyun.common.Rebuildable;
-import com.manong.weapon.aliyun.secret.DynamicSecret;
+import com.manong.weapon.aliyun.ons.ONSConsumer;
+import com.manong.weapon.aliyun.ons.ONSConsumerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.Properties;
 
 /**
  * ONS消息接收器
@@ -24,63 +22,11 @@ public class ONSReceiver extends Receiver implements Rebuildable {
     private final static Logger logger = LoggerFactory.getLogger(ONSReceiver.class);
 
     private ONSConsumerConfig consumerConfig;
-    private Consumer consumer;
+    private ONSProcessor processor;
+    private ONSConsumer consumer;
 
     public ONSReceiver(Map<String, Object> configMap) {
         super(configMap);
-    }
-
-    /**
-     * 构建消息接收器实例
-     *
-     * @return 构建成功返回true，否则返回false
-     */
-    private boolean build() {
-        Properties properties = new Properties();
-        properties.put(PropertyKeyConst.NAMESRV_ADDR, consumerConfig.serverURL);
-        properties.put(PropertyKeyConst.GROUP_ID, consumerConfig.consumeId);
-        properties.put(PropertyKeyConst.AccessKey, consumerConfig.aliyunSecret.accessKey);
-        properties.put(PropertyKeyConst.SecretKey, consumerConfig.aliyunSecret.secretKey);
-        properties.put(PropertyKeyConst.ConsumeThreadNums, consumerConfig.consumeThreadNum);
-        properties.put(PropertyKeyConst.MaxCachedMessageAmount, consumerConfig.maxCachedMessageNum);
-        properties.put(PropertyKeyConst.MessageModel, PropertyValueConst.CLUSTERING);
-        try {
-            consumer = ONSFactory.createConsumer(properties);
-            consumer.subscribe(consumerConfig.topic, consumerConfig.tags, (message, consumeContext) -> {
-                try {
-                    receiveProcessor.process(message);
-                    return Action.CommitMessage;
-                } catch (Throwable e) {
-                    logger.error("process message[{}] failed", message.getMsgID());
-                    logger.error(e.getMessage(), e);
-                    return Action.ReconsumeLater;
-                }
-            });
-            consumer.start();
-            logger.info("subscribe topic[{}] and tags[{}] success", consumerConfig.topic, consumerConfig.tags);
-            logger.info("build ONS receiver success");
-        } catch (Exception e) {
-            logger.error("build ONS receiver failed");
-            logger.error(e.getMessage(), e);
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public void rebuild() {
-        logger.info("ONS receiver is rebuilding ...");
-        if (DynamicSecret.accessKey.equals(consumerConfig.aliyunSecret.accessKey) &&
-                DynamicSecret.secretKey.equals(consumerConfig.aliyunSecret.secretKey)) {
-            logger.warn("secret is not changed, ignore ONS receiver rebuilding");
-            return;
-        }
-        consumerConfig.aliyunSecret.accessKey = DynamicSecret.accessKey;
-        consumerConfig.aliyunSecret.secretKey = DynamicSecret.secretKey;
-        Consumer prevConsumer = consumer;
-        if (!build()) throw new RuntimeException("rebuild ONS receiver failed");
-        if (prevConsumer != null) prevConsumer.shutdown();
-        logger.info("ONS receiver rebuild success");
     }
 
     @Override
@@ -88,8 +34,13 @@ public class ONSReceiver extends Receiver implements Rebuildable {
         logger.info("ONS receiver is starting ...");
         consumerConfig = JSON.toJavaObject(new JSONObject(configMap), ONSConsumerConfig.class);
         if (!consumerConfig.check()) return false;
-        if (!build()) return false;
-        RebuildManager.register(this);
+        if (receiveProcessor == null) {
+            logger.error("receive processor is null");
+            return false;
+        }
+        processor = new ONSProcessor(receiveProcessor);
+        consumer = new ONSConsumer(consumerConfig, processor);
+        if (!consumer.start()) return false;
         logger.info("ONS receiver has been started");
         return true;
     }
@@ -97,8 +48,7 @@ public class ONSReceiver extends Receiver implements Rebuildable {
     @Override
     public void stop() {
         logger.info("ONS receiver is stopping ...");
-        RebuildManager.unregister(this);
-        if (consumer != null) consumer.shutdown();
+        if (consumer != null) consumer.stop();
         logger.info("ONS receiver has been stopped");
     }
 }
